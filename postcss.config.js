71854b1912d97abd2ba2e7f9ee9bb028e3f0e94a
MrMJY <m1635836340@163.com>
2019-07-10 14:36:23 +0800

    '首页基本布局'

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,24 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*']
+// 通过判断修改 rem 适配
+
+const path = require('path')
+
+/**
+ * 打包工具在构建的时候，对样式文件的转换都会来调用这个函数
+ *    .css、.less 样式文件
+ *    .vue 单文件组件
+ * 我们可以在函数中针对不同的样式文件配置不同的转换规则
+ */
+module.export = (data) => {
+  // 如果是 node_moudels/vant 相关文件则设置为 37.5 否则设置为 70
+  const rootValue = data.file.dirname.indexOf(`node_modules${path.sep}vant`) !== -1 ? 37.5 : 70
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,36 @@
 <!-- home 组件 -->
 <template>
   <div class='home-wrap'>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
+    <header>
+      <van-search placeholder="搜索"
+                  background="#5babfb"
+                  input-align="center"
+                  v-model="value" />
+    </header>
+    <footer>
+      <van-tabbar v-model="active">
+        <van-tabbar-item icon="home">
+          <span>首页</span>
+          <i slot="icon"
+             class="iconfont icon-home f20"></i>
+        </van-tabbar-item>
+        <van-tabbar-item icon="search">
+          <span>问答</span>
+          <i slot="icon"
+             class="iconfont icon-i-message f20"></i>
+        </van-tabbar-item>
+        <van-tabbar-item icon="friends-o">
+          <span>视频</span>
+          <i slot="icon"
+             class="iconfont icon-video f20"></i>
+        </van-tabbar-item>
+        <van-tabbar-item icon="setting-o">
+          <span>我的</span>
+          <i slot="icon"
+             class="iconfont icon-Account f20"></i>
+        </van-tabbar-item>
+      </van-tabbar>
+    </footer>
   </div>
 </template>
 
@@ -15,7 +40,8 @@ export default {
   name: 'Home',
   data () {
     return {
-
+      value: '',
+      active: 0
     }
   },
   methods: {
@@ -24,4 +50,13 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+header {
+  background-color: #3296fa;
+  .van-search {
+    padding: 6px 42px;
+  }
+}
+.van-search__content {
+  border-radius: 17px;
+}
 </style>
